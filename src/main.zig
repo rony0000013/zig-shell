48700490db3997f6b430f6f4168ac772c994d32e
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2025-06-17 20:19:05 +0530

    added repl

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,18 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [4096]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+    while (true) {
+        try stdout.print("$ ", .{});
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [4096]u8 = undefined;
+        const user_input = (try stdin.readUntilDelimiterOrEof(&buffer, '\n')) orelse {
+            try stdout.print("\n", .{});
+            break;
+        };
 
-    const cmd = std.mem.trim(u8, user_input, &std.ascii.whitespace);
-    if (cmd.len == 0) return;
-    try stdout.print("{s}: command not found\n", .{cmd});
+        const cmd = std.mem.trim(u8, user_input, &std.ascii.whitespace);
+        if (cmd.len == 0) continue;
+        try stdout.print("{s}: command not found\n", .{cmd});
+    }
 }
